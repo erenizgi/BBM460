9dda44519dc3937256c4648964b7bc7ee7e11768
erenizgi <erenizgi001@gmail.com>
2025-04-29 14:55:46 +0300

    data request

--- wokwi-backend/index.js
+++ wokwi-backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const dataRoutes = require('./routes/dataRoutes');
+const fs = require("fs");
+const {all} = require("express/lib/application");
 
+global.datas = [];
+global.queue = 0;
 
 const app = express();
 const PORT = 3001;
-
 
 app.use(cors());
 app.use(express.json());
@@ -13,5 +16,29 @@ app.use(express.json());
 app.use('/api/sensorData', dataRoutes)
 
 app.listen(PORT, () => {
+    fs.readFile('./dummyInput.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        const dataArray = data.split("\n");//
+        let sanitized = dataArray.map((item) => item.replace("\r", ''));
+        sanitized = sanitized.filter((item) => item);
+        const allData = []
+        for (let i = 0; i < sanitized.length; i += 3) {
+            const temperature = sanitized[i].split(" ")[1];
+            const humidity = ((sanitized[i+1].split(" "))[1]).split("%")[0];
+            const light = sanitized[i+2].split(" ")[3];
+            const dataObj = {
+                "temperature" : temperature,
+                "humidity": humidity,//
+                "light": light,
+            };
+            allData.push(dataObj);
+            global.datas = allData
+        }
+
+
+    });
     console.log(`Server running at http://localhost:${PORT}`);
 });
--- wokwi-backend/routes/dataRoutes.js
+++ wokwi-backend/routes/dataRoutes.js
@@ -6,8 +6,18 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     try {
-
-        return res.status(200).json({});
+        fs.readFile('./dummyInput.txt', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        });
+        const data = global.datas;
+        res.status(200).json({"data": (global.datas)[global.queue++]});
+        if (global.queue === data.length){
+            global.queue = 0;
+        }
+        return;
     } catch (err) {
         return res.status(500).json({ error: 'Could not read sensor data' });
     }
